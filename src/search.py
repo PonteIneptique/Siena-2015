6f7d123649e75a6dfae622ca03054651a5225905
Thibault Clerice <leponteineptique@gmail.com>
2015-05-29 14:43:40 +0200

    Taking into account Externalisation

--- src/search.py
+++ src/search.py
@@ -1,16 +1,4 @@
-class ResultSet(object):
-  def __init__(self):
-    self.data = {}
-
-  def append(self, key, value):
-    self.data[key] = value
-
-  def toString(self):
-    s = []
-    for key in self.data:
-      for value in self.data[key]:
-        s.append("{0} : {1}".format(key, " ".join([v[1] for v in value])))
-    return "\n".join(s)
+from .result import ResultSet
 
 
 class Finder(object):
